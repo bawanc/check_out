753cd3e1ccb2a4356b2efabe103e4fe3bb88f563
bawanc <653031404@qq.com>
2019-09-24 23:52:11 +0800

    ni shisgedasaogua

---
+++ helloword.java
@@ -0,0 +1,11 @@
+package com.helloword.main;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("1");
+	System.out.println("Hello Word");
+	System.out.println("2");
+	System.out.println("Hello Word");
+    }
+}
